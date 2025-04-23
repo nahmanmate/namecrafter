4da5b2b0b842eadc934d04f750477f8a940f83ce
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-04-24 03:29:28 +1000

    initial commit:
    - Added name lists
    - Installed webpack

---
+++ src/nameGenerator.js
@@ -0,0 +1,6 @@
+// Copyright © 2025 James Johns - All Rights Reserved
+// Random Name Generator
+
+const firstNames = require('json-loader!.first-names.json');
+const middleNames = require('json-loader!.middle-names.json');
+const lastNames = require('json-loader!.last-names.json');
